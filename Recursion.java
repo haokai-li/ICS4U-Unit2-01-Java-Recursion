57fc4a747da605ab61f2ae0443df70d8526a4087
Ubuntu <ubuntu@ip-172-31-82-118.ec2.internal>
2021-12-02 01:40:15 +0000

    return error

---
+++ Recursion.java
@@ -0,0 +1,71 @@
+/*
+* The "Reverse the string" program, just proves you can show print to terminal.
+*
+* @author  Haokai
+*
+* @version 1.0
+*
+* @since   2021-12-01
+*
+*/
+
+import java.util.Scanner;
+
+/**
+* This is the standard "BoardFoot" program.
+*/
+final class Recursion {
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private Recursion() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting calculate function.
+    *
+    * @param originalString
+    *
+    * @return calculate
+    */
+
+    static String reverseProcess(final String originalString) {
+        if (originalString.isEmpty()) {
+            return originalString;
+        } else {
+            return reverseProcess(
+                originalString.substring(1)) + originalString.charAt(0);
+        }
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+
+    public static void main(final String[] args) {
+        try {
+            // input
+            final Scanner input = new Scanner(System.in);
+            System.out.print("The original string is : ");
+            final String originalString = input.nextLine();
+
+            final String reverse = reverseProcess(originalString);
+
+            System.out.println(
+                "The reversed string (using recursion) is : " + reverse);
+
+        } catch (java.util.InputMismatchException errorCode) {
+            System.out.println("Something was not a number.");
+        }
+        System.out.println("\nDone.");
+    }
+}
+
